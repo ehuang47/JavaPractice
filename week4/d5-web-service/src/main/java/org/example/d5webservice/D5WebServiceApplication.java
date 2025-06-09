b3bdeac991b45e4aa3f4d97d4200437a0a317378
ehuang47 <44561409+ehuang47@users.noreply.github.com>
2025-06-09 09:25:05 -0500

    Start new project for spring web services

---
+++ week4/d5-web-service/src/main/java/org/example/d5webservice/D5WebServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.d5webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D5WebServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(D5WebServiceApplication.class, args);
+  }
+
+}
